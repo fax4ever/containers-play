d746b5d01d3ba39c7e44ff1e89456d9894fd7143
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-06-19 19:59:39 +0200

    Try to use network docker ip address
    It does not work on my mac

--- test-containers/src/test/java/fax/play/container/AccessContainerTest.java
+++ test-containers/src/test/java/fax/play/container/AccessContainerTest.java
@@ -1,23 +1,36 @@
 package fax.play.container;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import redis.clients.jedis.Jedis;
+
 @Testcontainers
 public class AccessContainerTest {
 
+   public static final int ORIGINAL_PORT = 6379;
    @Container
-   public GenericContainer container = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
-         .withExposedPorts(6379);
+   public GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+         .withExposedPorts(ORIGINAL_PORT);
+
+   private Jedis jedis;
+
+   @BeforeEach
+   public void before() {
+      jedis = new Jedis(ipAddress(), redis.getMappedPort(ORIGINAL_PORT));
+   }
 
    @Test
    public void testSimplePutAndGet() {
-      assertThat(container.getHost()).isNotBlank();
-      assertThat(container.getFirstMappedPort()).isPositive();
+      jedis.hset("blablabla", "rage", "daoc");
+   }
+
+   public String ipAddress() {
+      // this works -> return redis.getContainerIpAddress()
+      return redis.getContainerInfo().getNetworkSettings().getNetworks().values().iterator().next().getIpAddress();
    }
 }
